fd571e578ec5d7e2d69b5868652c606351e9f431
penky776 <pohtato@protonmail.com>
2023-10-31 14:05:34 +0500

    clean up authentication

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
 use axum::{
-    body::{Empty, Full},
+    body::{boxed, Body, BoxBody, Full},
+    headers::Cookie,
     http::StatusCode,
     response::{Html, IntoResponse, Response},
     routing::get,
-    Form, Router,
+    Form, Router, TypedHeader,
 };
-use http::header::{LOCATION, SET_COOKIE};
+use http::{
+    header::{LOCATION, SET_COOKIE},
+    Request,
+};
 use serde::{Deserialize, Serialize};
 use std::{error::Error, fs::File, io::BufReader, path::Path};
+use tower::ServiceExt;
+use tower_http::services::ServeFile;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(home))
-        .route("/whoami", get(whoami))
+        .nest_service("/", get(home))
         .route("/login", get(login).post(authenticate_login))
         .route("/register", get(register).post(authenticate_register));
 
@@ -23,23 +28,55 @@ async fn main() {
         .unwrap();
 }
 
-async fn home() -> Html<&'static str> {
-    Html(std::include_str!("../assets/authenticated/home.html"))
+async fn home(TypedHeader(authenticated): TypedHeader<Cookie>) -> Response<BoxBody> {
+    let service_authenticated = ServeFile::new("assets/authenticated/home.html");
+    let service_unauthenticated = ServeFile::new("assets/unauthenticated/not_authenticated.html");
+
+    let auth = authenticated.get("authenticated");
+    if let Some(cookie) = auth {
+        if cookie == "yes" {
+            let res = service_authenticated
+                .oneshot(Request::new(Body::empty()))
+                .await
+                .unwrap();
+            return res.map(boxed);
+        }
+    }
+
+    let res = service_unauthenticated
+        .oneshot(Request::new(Body::empty()))
+        .await
+        .unwrap();
+    return res.map(boxed);
 }
 
-async fn login() -> Html<&'static str> {
-    Html(std::include_str!("../assets/login.html"))
+async fn login(TypedHeader(authenticated): TypedHeader<Cookie>) -> Html<&'static str> {
+    let auth = authenticated.get("authenticated");
+    if let Some(cookie) = auth {
+        if cookie == "yes" {
+            return Html(std::include_str!(
+                "../assets/authenticated/already_logged_in.html"
+            ));
+        }
+    }
+
+    return Html(std::include_str!("../assets/unauthenticated/login.html"));
 }
 
-async fn register() -> Html<&'static str> {
-    Html(std::include_str!("../assets/register.html"))
+async fn register(TypedHeader(authenticated): TypedHeader<Cookie>) -> Html<&'static str> {
+    let auth = authenticated.get("authenticated");
+    if let Some(cookie) = auth {
+        if cookie == "yes" {
+            return Html(std::include_str!(
+                "../assets/authenticated/already_logged_in.html"
+            ));
+        }
+    }
+
+    return Html(std::include_str!("../assets/unauthenticated/register.html"));
 }
 
-async fn whoami() -> Html<&'static str> {
-    Html(std::include_str!("../assets/not_authenticated.html"))
-}
-
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 struct User {
     username: String,
     password: String,
@@ -62,9 +99,9 @@ struct Comment {
 
 #[derive(Debug)]
 enum JsonData {
-    User(User), 
+    User(User),
     Post(Post),
-    Comment(Comment)
+    Comment(Comment),
 }
 
 fn read_user_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<User>, Box<dyn Error>> {
@@ -84,8 +121,9 @@ async fn authenticate_login(Form(user): Form<User>) -> impl IntoResponse {
             return Response::builder()
                 .status(StatusCode::SEE_OTHER)
                 .header(SET_COOKIE, "authenticated=yes")
+                .header(SET_COOKIE, "username=".to_owned() + &user.username)
                 .header(LOCATION, "/")
-                .body(Empty::new())
+                .body(Full::from("Logged in!"))
                 .unwrap();
         }
     }
@@ -93,8 +131,7 @@ async fn authenticate_login(Form(user): Form<User>) -> impl IntoResponse {
     return Response::builder()
         .status(StatusCode::SEE_OTHER)
         .header(SET_COOKIE, "authenticated=no")
-        .header(LOCATION, "/")
-        .body(Empty::new())
+        .body(Full::from("incorrect username or password!"))
         .unwrap();
 }
 
@@ -104,15 +141,17 @@ fn write_to_json_file<P: AsRef<Path>>(path: P, input: JsonData) -> Result<(), Bo
     match input {
         JsonData::Comment(comment) => {
             // TODO
-        },
+        }
         JsonData::Post(post) => {
             // TODO
-        },
+        }
         JsonData::User(user) => {
-            let mut prev_users: Vec<User> = serde_json::from_str(&existing_json).expect("Failed to deserialize JSON data");
+            let mut prev_users: Vec<User> =
+                serde_json::from_str(&existing_json).expect("Failed to deserialize JSON data");
             prev_users.push(user);
 
-            let updated_json = serde_json::to_string(&prev_users).expect("Failed to serialize data");
+            let updated_json =
+                serde_json::to_string(&prev_users).expect("Failed to serialize data");
             std::fs::write("users.json", updated_json).expect("Failed to write data to file");
         }
     }
@@ -127,16 +166,22 @@ async fn authenticate_register(Form(user): Form<User>) -> impl IntoResponse {
             return Response::builder()
                 .status(StatusCode::SEE_OTHER)
                 .header(SET_COOKIE, "authenticated=no")
-                .body(Full::from("username already exists! Please choose a different one."))
+                .body(Full::from(
+                    "username already exists! Please choose a different one.",
+                ))
                 .unwrap();
         }
     }
 
-    write_to_json_file("users.json", JsonData::User(user)).unwrap();
+    write_to_json_file("users.json", JsonData::User(user.clone())).unwrap();
 
     return Response::builder()
         .status(StatusCode::SEE_OTHER)
         .header(SET_COOKIE, "authenticated=yes")
-        .body(Full::from("logged in with your new account! Please redirect to the homepage."))
+        .header(SET_COOKIE, "username=".to_owned() + &user.username)
+        .header(LOCATION, "/")
+        .body(Full::from(
+            "logged in with your new account! Please redirect to the homepage.",
+        ))
         .unwrap();
 }
